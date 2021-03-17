4b040af3c59547ca2d4dce133f5d5a15af3b18ec
Mikhaella <social.amais@gmail.com>
2021-03-17 17:01:12 -0300

    📦 Start Login Screen

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,26 @@
+import styles from "../styles/pages/login.module.scss"
+
 export default function Auto() {
     return (
-        <div>
-            
+        <div className={styles.main}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="800" height="1800" viewBox="0 0 1122 2156.5">
+                <g id="Componente_10_1" data-name="Componente 10 – 1" transform="translate(2 2)">
+                <text id="Cash_Logo" data-name="Cash Logo" transform="translate(222 1901.5)" fill="none" stroke="#fff" stroke-width="2" font-size="1000" font-family="SegoeUI-BoldItalic, Segoe UI" font-weight="700" font-style="italic" letter-spacing="-0.228em"><tspan x="0" y="0">$</tspan></text>
+                <text id="Cash_Logo-2" data-name="Cash Logo" transform="translate(541 1079)" fill="none" stroke="#fff" stroke-width="2" font-size="1000" font-family="SegoeUI-BoldItalic, Segoe UI" font-weight="700" font-style="italic" letter-spacing="-0.228em"><tspan x="0" y="0">$</tspan></text>
+                <text id="Cash_Logo-3" data-name="Cash Logo" transform="translate(0 1185)" fill="none" stroke="#fff" stroke-width="2" font-size="1000" font-family="SegoeUI-BoldItalic, Segoe UI" font-weight="700" font-style="italic" letter-spacing="-0.228em"><tspan x="0" y="0">$</tspan></text>
+            </g>
+            <div>
+                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="388.5" height="217" viewBox="0 0 388.5 217">
+                    <g id="Logo_Write" data-name="Logo Write" transform="translate(17 730.81)">
+                    <text id="cash" transform="translate(-17 -554.81)" fill="#fff" font-size="163" font-family="SegoeUI-BoldItalic, Segoe UI" font-weight="700" font-style="italic"><tspan x="0" y="0">cash</tspan></text>
+                    <text id="Cash_Logo" data-name="Cash Logo" transform="translate(326 -632.81)" fill="none" stroke="#fff" stroke-width="0.5" font-size="77" font-family="SegoeUI-BoldItalic, Segoe UI" font-weight="700" font-style="italic" letter-spacing="-0.228em"><tspan x="0" y="0">$</tspan></text>
+                    </g>
+                </svg>
+                </div>
+                <div></div>
+            </div>
+        </svg>
         </div>
     )
   }
