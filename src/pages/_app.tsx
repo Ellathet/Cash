5a455a2651b3e788ea1b19d8a0fa2d83e75bea85
EllaThet <social.amais@gmail.com>
2021-03-17 21:01:19 -0300

    ⚙ Add O-Auths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { VisibilityProvider } from '../contexts/VisibilityContext';
 import '../styles/global.css';
+import { Provider } from 'next-auth/client'
 
 
 function MyApp({ Component, pageProps}) {
   return (
     <>
+    <Provider session={pageProps.session}>
     <VisibilityProvider>
       <Component {...pageProps} />
     </VisibilityProvider>
+    </Provider>
     </>
   )
 }
